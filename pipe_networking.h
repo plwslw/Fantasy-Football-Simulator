398842140c3f645e19697a7602ba39110b3f22a6
Steven Litvack-Winkler <s.lit.winkler@gmail.com>
2017-01-11 22:05:55 -0500

    ifndef loop

--- pipe_networking.h
+++ pipe_networking.h
@@ -1,3 +1,6 @@
+#ifndef PIPE_NETWORKING
+#define PIPE_NETWORKING 0
+
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <stdio.h>
@@ -6,7 +9,7 @@
 #include <unistd.h>
 #include <fcntl.h>
 
-#define MESSAGE_BUFFER_SIZE 256
+#define MESSAGE_BUFFER_SIZE 8192
 #define HANDSHAKE_BUFFER_SIZE 256
 
 int client_handshake(int* a);
@@ -14,3 +17,5 @@ int client_handshake(int* a);
 int server_handshake(int* a);
 int server_handshake1(char* buffer, int *a);
 int server_handshake2(char* buffer, int a);
+
+#endif
